23f85381f350b89d0c8350274b5437c48b6c699f
love543 <21cs2010@rgipt.ac.in>
2025-06-04 23:10:04 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Project',
+    version= '0.0.0',
+    author='Love Sharma',
+    author_email='lovesharma44349@gmail.com',
+    packages=find_packages(),
+    install_requires= []
+)
